3f0f9a1f7b51452309085279657903b75da8d114
shBLOCK <3332908658@qq.com>
2023-09-06 20:47:51 +0800

    Debug tests.yml: try to remove pyx from sources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 
 setup(
     ext_modules=[
-        Extension("gdmath.vector", ["src/gdmath/vector.pyx", "src/gdmath/vector.c"])
+        Extension("gdmath.vector", ["src/gdmath/vector.c"])
     ],
     packages=find_packages(
         where="src",
