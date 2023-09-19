2d505bb3c3d84c086ead7f08fc2def31a5754fe8
shBLOCK <3332908658@qq.com>
2023-09-19 11:42:13 +0800

    Introduce directives.pyx

---
+++ codegen/templates/directives.pyx
@@ -0,0 +1,10 @@
+#<TEMPLATE_BEGIN>
+#cython: language_level=3
+#cython: binding=False
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: nonecheck=False
+#cython: cdivision=True
+#cython: always_allow_keywords=True
+#cython: optimize.use_switch=True
+#<TEMPLATE_END>
--- codegen/templates/vector_template.pyx
+++ codegen/templates/vector_template.pyx
@@ -1,12 +1,5 @@
 #<TEMPLATE_BEGIN>
-#cython: language_level=3
-#cython: binding=False
-#cython: boundscheck=False
-#cython: wraparound=False
-#cython: nonecheck=False
-#cython: cdivision=True
-#cython: always_allow_keywords=True
-#cython: optimize.use_switch=True
+#<GEN>: from_template(open("templates/directives.pyx").read())
 
 cimport cython
 from libc.math cimport sqrt
